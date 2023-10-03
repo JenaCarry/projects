185f083c8f3a6bd760cb4d81ebb97f3f0684bfb0
Jean <120676537+JenaCarry@users.noreply.github.com>
2023-10-02 21:20:03 -0300

    todo list

---
+++ todo-list/README.md
@@ -0,0 +1,15 @@
+<div align="center">
+  <h2>Todo List</h2>
+</div>
+
+## Screenshot
+
+<div align="center">
+
+![](./assets/images/screenshot.png)
+
+</div>
+
+- Live Site URL: [https://jenacarry.github.io/projects/todo-list/](https://jenacarry.github.io/projects/todo-list/)
+
+# Finalizando
---
+++ todo-list/assets/js/index.js
@@ -0,0 +1,70 @@
+const todoList = document.querySelector(".todo-list");
+const form = document.querySelector("form");
+const createTodo = document.querySelector("#create-todo");
+
+// const todos = [];
+
+const getAllTodos = () => JSON.parse(localStorage.getItem("todos")) ?? [];
+
+const setAllTodos = (todos) =>
+  localStorage.setItem("todos", JSON.stringify(todos));
+
+const clearAllTodos = () => {
+  todoList.innerHTML = "";
+};
+
+const updateDisplay = () => {
+  clearAllTodos();
+  const todos = getAllTodos();
+  todos.map((todo, index) => createTodoItem(todo, index));
+};
+
+const completedTodo = (index) => {
+  const todos = getAllTodos();
+  todos[index].isCompleted = todos[index].isCompleted === false ? true : false;
+  setAllTodos(todos);
+  updateDisplay();
+};
+
+const deletedTodo = (index) => {
+  const todos = getAllTodos();
+  todos.splice(index, 1);
+  setAllTodos(todos);
+  updateDisplay();
+};
+
+const createTodoItem = (todo, index) => {
+  const liElement = document.createElement("li");
+  liElement.classList = `todo-item ${todo.isCompleted ? "completed" : ""}`;
+  liElement.innerHTML = `
+    <div class="todo-check">
+      <ion-icon name="checkmark-outline"></ion-icon>
+    </div>
+    <h2>${todo.text}</h2>
+    <button class="xmark">X</button>`;
+  todoList.appendChild(liElement);
+  liElement.addEventListener("click", (e) => {
+    if (e.target.classList.contains("xmark")) {
+      deletedTodo(index);
+    } else {
+      completedTodo(index);
+    }
+  });
+};
+
+const createNewTodo = (e) => {
+  if (e.key === "Enter") {
+    if (createTodo.value !== "") {
+      const todos = getAllTodos();
+      todos.push({ text: createTodo.value, isCompleted: false });
+      setAllTodos(todos);
+      updateDisplay();
+      createTodo.value = "";
+    }
+  }
+};
+
+form.addEventListener("submit", (e) => e.preventDefault());
+createTodo.addEventListener("keypress", createNewTodo);
+
+updateDisplay();
